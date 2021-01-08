3cc0abac0f99c2e7b5edb3bcc1829dd59e309963
misleidy ramirez <misleidy520@gmail.com>
2021-01-08 14:29:53 -0300

    cambio api url

--- src/components/helpers/getGifs.js
+++ src/components/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async (category) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=U6sggE1TNi9ItTiLfPSYxwPIwYfHkxoZ`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=U6sggE1TNi9ItTiLfPSYxwPIwYfHkxoZ`
     const resp = await fetch(url);
     const { data } = await resp.json();
 
