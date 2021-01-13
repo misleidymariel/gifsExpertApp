05e12f1538312d080eceec77358b2a9fa5086f08
misleidy ramirez <misleidy520@gmail.com>
2021-01-13 19:00:00 -0300

    usando waitForNextUpdate en hook

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -4,18 +4,31 @@ import { renderHook } from "@testing-library/react-hooks";
 
 describe('Pruebas en hook <useFetchGifs/>', () => {
 
-    test('debe de retornar el estado inicial ', () => {
+    test('debe de retornar el estado inicial ', async () => {
 
-         const { result } =renderHook( () => useFetchGifs( 'One Punch') );
+         const { result, waitForNextUpdate } =renderHook( () => useFetchGifs( 'One Punch') );
          const { data, loading } = result.current;
 
-         console.log( data, loading);
-
-
-       //const { data , loading} = useFetchGifs( 'One Punch');
-       
+         await waitForNextUpdate();
+         //console.log( data, loading);
+         expect( data).toEqual([]);
+         expect( loading).toBe( true);
+           //const { data , loading} = useFetchGifs( 'One Punch');  
         
     })
     
+    test('debe de retornar un arreglo de imgs y el loading en false ', async() => {
+      //waitForNextUpdate funsion que retorna una promesa no retorna nada
+
+      const { result, waitForNextUpdate} = renderHook( () => useFetchGifs('One punch'));
+      await waitForNextUpdate();
+
+      const { data, loading } = result.current;
+
+      expect( data.length).toBe( 10);
+      expect( loading).toBe( false);
+    })
     
+
+
 })
